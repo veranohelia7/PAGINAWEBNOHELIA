2fc2e6d9a3304db7661711007a1cee8bd2c39bb0
veranohelia7 <noheliaveraalarcon224@gmail.com>
2024-07-09 11:09:48 -0500

    Add files via upload

---
+++ PAGINA WEB/efecto1.js
@@ -0,0 +1,38 @@
+/*creamos un arreglo de imagenes */
+var imagenes= ["PASTEL 1.jpg","PASTEL 2.jpg","PASTEL 3.jpg","PASTEL 4.jpg"]
+/*mostramos la primera imagen para el slider */
+document.Imagen.src=imagenes[0];
+/* hacemos referencia a los botones derecho e izquierdo con queryselector*/
+var sliderderecha= document.querySelector(".slider-derecho");
+var sliderizquierda=document.querySelector(".slider-izquierdo");
+/*declaramos un contador para las transiciones */
+var contador=0;
+
+/*implementamos la funcion mover derecha*/
+function moverderecha()
+{
+    contador++; /*el contador se incrementa */
+    if(contador>imagenes.length-1)/*con la condicion reiniciamo el slider */
+        {
+            contador=0;  /*el contador se pone en cero otra vez*/
+        }
+    document.Imagen.src=imagenes[contador];/*mostrando de acuerdo al indice*/
+}
+/*este es el evento cuando hacemos clic las imagenes se mueven a la derecha */
+sliderderecha.addEventListener("click",moverderecha)
+
+/*transicion automatica del slider*/
+setInterval(moverderecha,5000);
+
+/*implementamos la funcion mover izquierda*/
+function moverizquierda()
+{
+    contador--; /*el contador desminuye */
+    if(contador<0)/*salvamos el indexado no existe posicion -1 */
+        {
+            contador=imagenes.length-1;/*el contador vuelve a 3 */
+        }
+        document.Imagen.src=imagenes[contador];/*mostrando de acuerdo al indice*/
+}
+/*este es el evento cuando hacemos clic las imagenes se mueven a la derecha */
+sliderizquierda.addEventListener("click",moverizquierda);
